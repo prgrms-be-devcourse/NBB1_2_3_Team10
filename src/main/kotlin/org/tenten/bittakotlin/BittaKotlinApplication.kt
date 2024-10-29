3373111fb8d5d7aeb77914197a36e776dcb88af4
deveunhwa <deveunhwa@gmail.com>
2024-10-29 11:52:04 +0900

    Chore: 기본 구조 설정

---
+++ src/main/kotlin/org/tenten/bittakotlin/BittaKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.tenten.bittakotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BittaKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<BittaKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/org/tenten/bittakotlin/BittaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.tenten.bittakotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BittaKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
