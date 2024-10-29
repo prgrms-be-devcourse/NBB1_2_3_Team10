116083d82d369057fd563e84b4de1258887fb1a9
Preta3418 <jason123qwe@gmail.com>
2024-10-29 12:20:47 +0900

    Feat: Scout Added

---
+++ src/main/kotlin/org/tenten/bittakotlin/scout/dto/ScoutDTO.java
@@ -0,0 +1,21 @@
+package org.tenten.bittakotlin.scout.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoutDTO {
+    private Long id;
+    private Long feedId;
+    private Long senderId;
+    private Long receiverId;
+    private String description;
+    private LocalDateTime sentAt;
+}
