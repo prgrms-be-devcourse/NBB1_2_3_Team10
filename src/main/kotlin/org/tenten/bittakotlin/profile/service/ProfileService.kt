addf4a102af8f042da3f7e71366658c93d8188a1
juwon-code <juwoncode@gmail.com>
2024-11-01 17:10:47 +0900

    Feat: 프로필 객체 조회 함수 추가
    닉네임으로 프로필 객체를 조회하기 위해 필요한 함수를 구현합니다.
    
    Related to: prgrms-be-devcourse/NBB1_2_3_Team10#9

--- src/main/kotlin/org/tenten/bittakotlin/profile/service/ProfileService.kt
+++ src/main/kotlin/org/tenten/bittakotlin/profile/service/ProfileService.kt
@@ -2,10 +2,13 @@ package org.tenten.bittakotlin.profile.service
 
 import org.tenten.bittakotlin.member.entity.Member
 import org.tenten.bittakotlin.profile.dto.ProfileDTO
+import org.tenten.bittakotlin.profile.entity.Profile
 
 interface ProfileService {
     fun createProfile(ProfileDTO: ProfileDTO): ProfileDTO
     fun getProfile(memberId: Long): ProfileDTO
     fun updateProfile(memberId: Long, profileDTO: ProfileDTO): ProfileDTO
     fun createDefaultProfile(member: Member): ProfileDTO
+
+    fun getByNickname(nickname: String): Profile
 }
