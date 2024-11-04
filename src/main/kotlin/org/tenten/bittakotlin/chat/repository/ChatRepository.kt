f30c706d43b918658eedb87a9c105a40cfaf38a0
juwon-code <juwoncode@gmail.com>
2024-11-05 02:40:09 +0900

    Feat: 채팅 조회 슬라이싱 적용
    채팅 조회에 슬라이싱을 적용하고, 피드 레포지토리의 ORDER BY가 누락된 조회 쿼리문을 수정했습니다.
    
    Related to: prgrms-be-devcourse/NBB1_2_3_Team10#28

--- src/main/kotlin/org/tenten/bittakotlin/chat/repository/ChatRepository.kt
+++ src/main/kotlin/org/tenten/bittakotlin/chat/repository/ChatRepository.kt
@@ -1,5 +1,7 @@
 package org.tenten.bittakotlin.chat.repository
 
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -8,6 +10,6 @@ import org.tenten.bittakotlin.chat.entity.Chat
 
 @Repository
 interface ChatRepository : JpaRepository<Chat, Long> {
-    @Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :chatRoomId")
-    fun findAllByChatRoomId(@Param("chatRoomId") chatRoomId: Long): List<Chat>
+    @Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :chatRoomId ORDER BY c.id DESC")
+    fun findAllByChatRoomIdOrderByIdDesc(@Param("chatRoomId") chatRoomId: Long, pageable: Pageable): Slice<Chat>
 }
--- src/main/kotlin/org/tenten/bittakotlin/chat/service/ChatService.kt
+++ src/main/kotlin/org/tenten/bittakotlin/chat/service/ChatService.kt
@@ -1,10 +1,11 @@
 package org.tenten.bittakotlin.chat.service
 
+import org.springframework.data.domain.Pageable
 import org.tenten.bittakotlin.chat.dto.ChatRequestDto
 import org.tenten.bittakotlin.chat.dto.ChatResponseDto
 
 interface ChatService {
-    fun get(requestDto: ChatRequestDto.Read): List<ChatResponseDto.Read>
+    fun get(pageable: Pageable, requestDto: ChatRequestDto.Read): List<ChatResponseDto.Read>
 
     fun save(requestDto: ChatRequestDto.Send): ChatResponseDto.Send
 
