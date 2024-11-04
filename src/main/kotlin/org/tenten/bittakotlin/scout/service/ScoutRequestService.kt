4a9c6794c52ec827c3ef3eeb8bda03bc42246429
Preta3418 <jason123qwe@gmail.com>
2024-11-04 12:40:34 +0900

    Refactoring: feed,profile 연결 코드 수정
    FeedProvider 가 삭제 되었기에 FeedRepository 로 연결 하였습니다. Member 또한 Profile 을 통해 연결하기로 한 고로 Profile 로 전부 변환 완료 하였습니다
    
    Related to: prgrms-be-devcourse#2

--- src/main/kotlin/org/tenten/bittakotlin/scout/service/ScoutRequestService.kt
+++ src/main/kotlin/org/tenten/bittakotlin/scout/service/ScoutRequestService.kt
@@ -1,26 +1,24 @@
 package org.tenten.bittakotlin.scout.service
-/*
-//import com.prgrms2.java.bitta.feed.entity.Feed
-//import com.prgrms2.java.bitta.member.entity.Member
-//import com.prgrms2.java.bitta.member.service.MemberProvider
-//import com.prgrms2.java.bitta.feed.service.FeedProvider
+
+
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.tenten.bittakotlin.member.repository.MemberRepository
+import org.tenten.bittakotlin.feed.repository.FeedRepository
+import org.tenten.bittakotlin.profile.repository.ProfileRepository
 import org.tenten.bittakotlin.scout.dto.ScoutDTO
 import org.tenten.bittakotlin.scout.entity.ScoutRequest
 import org.tenten.bittakotlin.scout.repository.ScoutRequestRepository
-import java.lang.reflect.Member
 
 @Service
 class ScoutRequestService(
     private val scoutRequestRepository: ScoutRequestRepository,
-   // private val feedProvider: FeedProvider,
-    private val memberRepository: MemberRepository
+    private val feedRepository: FeedRepository,
+    private val profileRepository: ProfileRepository
 ) {
+    
 
     @Transactional
     fun sendScoutRequest(scoutDTO: ScoutDTO): ScoutDTO {
@@ -31,39 +29,40 @@ class ScoutRequestService(
 
     @Transactional(readOnly = true)
     fun getSentScoutRequests(senderId: Long, pageable: Pageable): Page<ScoutDTO> {
-        return scoutRequestRepository.findBySenderIdOrderById(senderId, pageable)
+        return scoutRequestRepository.findBySender_IdOrderById(senderId, pageable)
             .map { request -> entityToDto(request) }
     }
 
     @Transactional(readOnly = true)
     fun getReceivedScoutRequests(receiverId: Long, pageable: Pageable): Page<ScoutDTO> {
-        return scoutRequestRepository.findByReceiverIdOrderById(receiverId, pageable)
+        return scoutRequestRepository.findByReceiver_IdOrderById(receiverId, pageable)
             .map { request -> entityToDto(request) }
     }
 
     private fun entityToDto(request: ScoutRequest): ScoutDTO {
-       // return ScoutDTO(
-           // id = request.id,
-          //  senderId = request.sender?.id ?: throw IllegalStateException("Sender ID is missing"),
-            //receiverId = request.receiver?.id ?: throw IllegalStateException("Receiver ID is missing"),
-           // description = request.description,
-            //sentAt = request.sentAt
+        return ScoutDTO(
+            id = request.id,
+            feedId = request.feed.id ?: throw IllegalStateException("Feed ID is missing"),
+            senderId = request.sender.id ?: throw IllegalStateException("Sender Profile ID is missing"),
+            receiverId = request.receiver.id ?: throw IllegalStateException("Receiver Profile ID is missing"),
+            description = request.description,
+            sentAt = request.sentAt
         )
     }
 
     private fun dtoToEntity(scoutDTO: ScoutDTO): ScoutRequest {
-       // val feed = feedRepository.findById(scoutDTO.feedId)
-         //   .orElseThrow { EntityNotFoundException("Feed not found with id=${scoutDTO.feedId}") }
+        val feed = feedRepository.findById(scoutDTO.feedId)
+            .orElseThrow { EntityNotFoundException("Feed not found with id=${scoutDTO.feedId}") }
 
-        val sender = memberRepository.findById(scoutDTO.senderId)
+        val sender = profileRepository.findById(scoutDTO.senderId)
             .orElseThrow { EntityNotFoundException("Sender not found with id=${scoutDTO.senderId}") }
 
-        val receiver = memberRepository.findById(scoutDTO.receiverId)
+        val receiver = profileRepository.findById(scoutDTO.receiverId)
             .orElseThrow { EntityNotFoundException("Receiver not found with id=${scoutDTO.receiverId}") }
 
         return ScoutRequest(
             id = scoutDTO.id,
-          //  feed = feed,
+            feed = feed,
             sender = sender,
             receiver = receiver,
             description = scoutDTO.description,
@@ -72,4 +71,3 @@ class ScoutRequestService(
     }
 }
 
- */
