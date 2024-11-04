3697a5870080cd8d6c35c6954acc5cc07905e91b
Preta3418 <jason123qwe@gmail.com>
2024-11-04 17:16:07 +0900

    Feat: nickname 생성 구현
    닉네임 자동 생성 + update 기능을 구현했습니다. 추가로 memberId 참조를 전부 profileId 참조로 변경했습니다
    
    Related to: prgrms-be-devcourse#10

--- src/main/kotlin/org/tenten/bittakotlin/profile/service/ProfileService.kt
+++ src/main/kotlin/org/tenten/bittakotlin/profile/service/ProfileService.kt
@@ -5,9 +5,8 @@ import org.tenten.bittakotlin.profile.dto.ProfileDTO
 import org.tenten.bittakotlin.profile.entity.Profile
 
 interface ProfileService {
-    fun createProfile(ProfileDTO: ProfileDTO): ProfileDTO
-    fun getProfile(memberId: Long): ProfileDTO
-    fun updateProfile(memberId: Long, profileDTO: ProfileDTO): ProfileDTO
+    fun getProfile(profileId: Long): ProfileDTO
+    fun updateProfile(profileId: Long, profileDTO: ProfileDTO): ProfileDTO
     fun createDefaultProfile(member: Member): ProfileDTO
 
     fun getByNickname(nickname: String): Profile
