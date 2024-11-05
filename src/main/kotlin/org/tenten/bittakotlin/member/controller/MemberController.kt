d5fe994e01bddfbbb9d21a0d3f18561d63712fab
ghtndl <leesooho0406@gmail.com>
2024-11-05 16:30:06 +0900

    Feat: Member Swagger

--- src/main/kotlin/org/tenten/bittakotlin/member/controller/MemberController.kt
+++ src/main/kotlin/org/tenten/bittakotlin/member/controller/MemberController.kt
@@ -1,5 +1,10 @@
 package org.tenten.bittakotlin.member.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.AccessDeniedException
@@ -11,6 +16,7 @@ import org.tenten.bittakotlin.member.repository.MemberRepository
 import org.tenten.bittakotlin.member.service.MemberService
 import org.tenten.bittakotlin.security.jwt.JWTUtil
 
+@Tag(name = "회원관리 API 컨트롤러", description = "회원과 관련된 RestAPI 제공 컨트롤러")
 @RestController
 @RequestMapping("/api/v1/member")
 class MemberController(
@@ -20,6 +26,26 @@ class MemberController(
 ) {
 
     // 회원가입
+
+    @Operation(
+        summary = "회원가입",
+        description = "회원가입을 진행합니다.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "회원가입 성공",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(example = "MEMBER_SUCCESS_SIGN_UP") // 예시 값은 문자열로 처리
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "회원가입 실패",
+                content = [Content()]
+            )
+        ]
+    )
     @PostMapping("/join")
     fun join(@RequestBody joinDTO: MemberRequestDTO.Join): ResponseEntity<MemberRequestDTO.Join> {
         memberService.join(joinDTO)
@@ -27,6 +53,16 @@ class MemberController(
     }
 
     // 회원 정보 조회
+
+    @Operation(
+        summary = "회원 정보 조회",
+        description = "회원의 ID를 사용해 회원 정보를 조회합니다.",
+        responses = [ApiResponse(
+            responseCode = "200",
+            description = "회원 정보 조회 성공",
+            content = [Content(mediaType = "application/json")]
+        ), ApiResponse(responseCode = "404", description = "회원 정보 조회 실패", content = [Content()])]
+    )
     @GetMapping("/{id}")
     fun read(@PathVariable id: Long): ResponseEntity<MemberResponseDTO.Information> {
         val memberInfo = memberService.read(id)
